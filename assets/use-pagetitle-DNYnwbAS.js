58e38ae321b295154efef844003fab928b59fd3f
JeffreyWang060303 <JeffreyWang060303@users.noreply.github.com>
2026-01-27 04:44:21 +0000

    [ci] deploy site 0ba6fb9f134ed62afe5dc56adb6d57e01c5c72f3

---
+++ assets/use-pagetitle-DNYnwbAS.js
@@ -0,0 +1,1 @@
+import{r as t,ad as a}from"./index-hy99Ff0C.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${a.name}`},[e])}export{o as u};
--- assets/use-pagetitle-XUgKpsdu.js
+++
@@ -1,1 +0,0 @@
-import{r as t,ac as a}from"./index-CxN8iyIG.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${a.name}`},[e])}export{o as u};

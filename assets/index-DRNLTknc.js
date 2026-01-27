d1a39c6580cd150d2cf3c21cc63699e9f27384cf
JeffreyWang060303 <JeffreyWang060303@users.noreply.github.com>
2026-01-27 05:16:19 +0000

    [ci] deploy site 9d544ec27ee0cbf41a62f03ca45457d2dca5a805

--- assets/index-DRNLTknc.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CtsfGqf1.js";import{u as i}from"./use-pagetitle-PzulW02j.js";import{T as a}from"./teaching-B-f-hp-6.js";import"./table-CGOnEQlE.js";function l(){return i("Teaching Experience"),e.jsx("div",{className:"flex flex-1 flex-col items-center gap-10",children:e.jsx("div",{className:"w-full max-w-5xl px-2 md:px-8",children:e.jsx(a,{})})})}export{l as default};
---
+++ assets/use-pagetitle-BHOo6LuN.js
@@ -0,0 +1,1 @@
+import{r as t,aj as a}from"./index-BV9s3UtC.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${a.name}`},[e])}export{o as u};

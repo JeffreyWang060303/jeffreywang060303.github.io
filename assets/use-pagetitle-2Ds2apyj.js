dac50e55ad3f9755d7deed458598cd04e7951a9e
JeffreyWang060303 <JeffreyWang060303@users.noreply.github.com>
2026-01-27 04:18:43 +0000

    [ci] deploy site 35f8ed0054db4877f9cd6e20e5f57aefb0195782

---
+++ assets/use-pagetitle-2Ds2apyj.js
@@ -0,0 +1,1 @@
+import{r as t,ac as a}from"./index-BrFf7boH.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${a.name}`},[e])}export{o as u};
--- assets/use-pagetitle-D4s_27kV.js
+++
@@ -1,1 +0,0 @@
-import{r as t,aa as a}from"./index-DW7BfMrv.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${a.name}`},[e])}export{o as u};

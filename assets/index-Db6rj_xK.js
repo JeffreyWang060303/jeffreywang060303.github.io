f7caf827e0a96a4f2d6022eb207cd8ee3a6be8b0
JeffreyWang060303 <JeffreyWang060303@users.noreply.github.com>
2026-08-21 05:11:43 +0000

    [ci] deploy site acb5f1891e02551c24b11cbb8677a1efabc3114c

---
+++ assets/index-Db6rj_xK.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BOwSqW_B.js";import{u as a}from"./use-pagetitle-B8LmHaoU.js";import{T as i}from"./teaching-C0HDoB2o.js";function l(){return a("Teaching Experience"),e.jsx("div",{className:"flex flex-1 flex-col items-center gap-10",children:e.jsx("div",{className:"w-full max-w-5xl px-2 md:px-8",children:e.jsx(i,{})})})}export{l as default};
---
+++ assets/use-pagetitle-B8LmHaoU.js
@@ -0,0 +1,1 @@
+import{r as t,ah as a}from"./index-BOwSqW_B.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${a.name}`},[e])}export{o as u};

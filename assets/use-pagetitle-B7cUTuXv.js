220de24ab2e60b2b5a9621797469b5e8a0d7c489
JeffreyWang060303 <JeffreyWang060303@users.noreply.github.com>
2026-09-04 18:24:16 +0000

    [ci] deploy site 5b434633a7587a310c2d06090b7df3f580c7f07e

---
+++ assets/use-pagetitle-B7cUTuXv.js
@@ -0,0 +1,1 @@
+import{r as t,ai as a}from"./index-uPEaysO0.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${a.name}`},[e])}export{o as u};
--- assets/use-pagetitle-YgomovKm.js
+++
@@ -1,1 +0,0 @@
-import{r as t,ai as a}from"./index-34AAuBoT.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${a.name}`},[e])}export{o as u};
